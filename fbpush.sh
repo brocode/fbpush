459a805982447a9dd22c6fe17d82ccec3790a54a
Maximilien Riehl <max@flatmap.ninja>
2017-08-16 16:01:35 +0200

    [feat] make script more resilient to weird environments

--- fbpush.sh
+++ fbpush.sh
@@ -4,8 +4,13 @@ set -e -u -o pipefail
 BRANCH_NAME=${1:? "no branch name specified, usage: $0 <branch> <msg>"}
 MSG=${2:? "no message specified, usage: $0 <branch> <msg>"}
 
+command -v hub >/dev/null 2>&1 || {
+    echo "You need to install hub (https://github.com/github/hub) and it must be in your path."
+    exit 1
+}
+
 git commit -m "$MSG"
-git co -b $BRANCH_NAME
+git checkout -b $BRANCH_NAME
 git push origin $BRANCH_NAME:$BRANCH_NAME
 
 URL=$(hub pull-request -m "$MSG" | tr -d "\n")
@@ -34,5 +39,6 @@ xdg-open "$URL"
 echo "I have opened the pull request page. Please click on merge there and delete the remote branch."
 echo "     See https://github.com/github/hub/issues/1483 for context - this can't be automated with hub yet."
 
-git co master
+git checkout master
+git remote update
 git branch -d $BRANCH_NAME
